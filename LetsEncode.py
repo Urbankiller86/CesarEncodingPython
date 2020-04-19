5605b328ce2cc27b3e72ee4507ab0a73475da5c7
Urbankiller86 <re-boot-to-fix@outlook.es>
2020-04-19 13:48:19 -0500

    Bugs fixed

--- LetsEncode.py
+++ LetsEncode.py
@@ -1,18 +1,33 @@
 from encodingFuctions import encode
 from encodingFuctions import decode
+import os.path
 
 location=input("Insert file path: ")
-with open(location, 'r') as file:
-    data = file.read().replace('\n', '')
-option=input("1 to encode. Other number to decode: ")
-if int(option) == 1:
-    data=data.upper()
-    secretMessage=encode(data)
-    writer=open(location,"w+")
-    writer.write(secretMessage)
-    writer.close()
-else:
-    originalMessage=decode(data)
-    writer=open(location,"w+")
-    writer.write(originalMessage)
-    writer.close
+checkLocation = True
+while checkLocation:
+    if os.path.isfile(location):
+        with open(location, 'r') as file:
+            data = file.read().replace('\n', '')
+        checkLocation=False
+    else:
+        location=input("Insert a valid path")
+        
+checkNumber=True
+while checkNumber:
+    try:
+        option=input("1 to encode. Other number to decode: ")
+        if int(option) == 1:
+            data=data.upper()
+            secretMessage=encode(data)
+            writer=open(location,"w+")
+            writer.write(secretMessage)
+            writer.close()
+            checkNumber=False
+        else:
+            originalMessage=decode(data)
+            writer=open(location,"w+")
+            writer.write(originalMessage)
+            writer.close
+        checkNumber=False
+    except:
+        checkNumber=True
