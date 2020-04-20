3f69cba6759e035aacbd0ba3f364a3bf3711def3
Urbankiller86 <re-boot-to-fix@outlook.es>
2020-04-20 16:30:47 -0500

    Uploading to Github

---
+++ evalFunction.py
@@ -0,0 +1,4 @@
+evaluate=input("Write a simple function")
+x=2
+print(evaluate)
+print(eval(evaluate))
---
+++ valueSwap.py
@@ -0,0 +1,4 @@
+variable1=1
+variable2=2
+variable1,variable2=variable2,variable1
+print(variable1,variable2)
